8c9bca5c30585ec91c12340d6afcbde5ff7c555e
alanjvano <46762315+alanjvano@users.noreply.github.com>
2019-03-19 15:47:58 -0500

    added f2l pos

--- f2lModule.py
+++ f2lModule.py
@@ -1,6 +1,8 @@
 import cube_info
 import cubeModule
 
+import copy
+
 # define f2l algorithms
 f2l_alg = [
 	# Case 1: (1-8) corner and edge on top, cross corner oriented FR, same color on top face
@@ -20,3 +22,38 @@ f2l_alg = [
 	
 	# Case 6: (37-41) corner bottom layer, edge middle layer
         [RuruRUrU2Rur],[RUrU2RurURUr],[fUFU2RUR2FRf],[RurU2fuF2rfR],[rFRfRurURurU2Rur]]
+
+# define positions of 47 (corner) and 23 (edge piece) to match with f2l case
+f2l_pos = [
+        # Case 1
+        [[20,5],[27,19],[20,1],[27,10],[20,3],[27,37],[20,7],[27,28],
+        # Case 2
+        [20,10],[27,1],[20,37],[27,3],[20,28],[27,7],[20,19],[27,5],
+        # Case 3
+        [8,5],[8,19],[8,1],[8,10],[8,3],[8,37],[8,7],[8,28],
+        # Case 4
+        [47,19],[47,5],[26,19],[33,5],[26,5],[33,19],
+        # Case 5
+        [20,23],[27,23],[20,30],[27,30],[8,30],[8,23],
+        # Case 5
+        [26,23],[33,23],[26,30],[33,30],[47,30]]
+
+# for each four corner pairs, find the location of the corresponding edge and corner piece
+# then, based on their location implement the correct f2l algorithm
+# rotate the cube until complete
+def solveF2l(cube_in):
+    tmp_cube = cubeModule.Cube()
+    tmp_cube.cube = copy.deepcopy(cube_in.cube)
+    for i in range(4):
+        
+        # find edge piece
+
+
+        # find corner piece
+
+        # permutate with corresponding algorithm
+
+       
+
+    
+    
